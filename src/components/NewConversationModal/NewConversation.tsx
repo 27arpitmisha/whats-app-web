dfb2616face9c4ffbd4afb77f31352bec2aa7f8d
Arpit <arpit@test.com>
2022-01-19 12:48:09 +0530

    Add conversation list with contact names displayed

--- src/components/NewConversationModal/NewConversation.tsx
+++ src/components/NewConversationModal/NewConversation.tsx
@@ -1,5 +1,51 @@
-import { Modal } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import Contact from "../../Models/ContactsModel";
+import { RootState } from "../../redux/reducers";
 
 export default function NewConversation() {
-  return <Modal.Header closeButton> New Conversations </Modal.Header>;
+  const [selectedContactIds, setselectedContactIds] = useState<string[]>([]);
+  const contacts = useSelector((state: RootState) => state.contactReducer);
+
+  const onSubmitConversationsHandle = () => {
+    console.log(selectedContactIds);
+  };
+  const handleChangeID = (contactId: string) => {
+    setselectedContactIds((previoudId) => {
+      if (previoudId.includes(contactId)) {
+        return previoudId.filter((prevIds) => {
+          return contactId !== prevIds;
+        });
+      } else {
+        return [...selectedContactIds, contactId];
+      }
+    });
+  };
+  return (
+    <>
+      <Modal.Header closeButton>New Contacts</Modal.Header>
+      <Modal.Body>
+        <Form>
+          {contacts.map((contact: Contact) => {
+            return (
+              <Form.Group controlId={contact.id} key={contact.id}>
+                <Form.Check
+                  type="checkbox"
+                  value={selectedContactIds.includes(contact.id).toString()}
+                  onChange={() => {
+                    handleChangeID(contact.id);
+                  }}
+                  label={contact.name}
+                />
+              </Form.Group>
+            );
+          })}
+        </Form>
+        <Button variant="primary" onClick={onSubmitConversationsHandle}>
+          Start conversation
+        </Button>
+      </Modal.Body>
+    </>
+  );
 }
